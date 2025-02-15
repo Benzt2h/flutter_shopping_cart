bc5f472206d1b45e6bb239b05489dd273f3e01b0
Nattapong Wongtakai <nattapong@emetworks.com>
2025-02-15 14:15:22 +0700

    add bottom nav bar

--- lib/modules/home/page/home_page.dart
+++ lib/modules/home/page/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 import 'package:refreshed/refreshed.dart';
+import 'package:shopping_cart/modules/shopping/page/shopping_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,12 +9,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-            onTap: () {
-              Get.toNamed("/cart");
+      body: PersistentTabView(
+        tabs: [
+          PersistentTabConfig(
+            screen: ShoppingPage(),
+            item: ItemConfig(
+              icon: Icon(Icons.home),
+              title: "Shopping",
+            ),
+          ),
+          PersistentTabConfig(
+            screen: SizedBox.shrink(),
+            item: ItemConfig(
+              icon: Icon(Icons.shopping_cart),
+              title: "Cart",
+            ),
+          ),
+        ],
+        navBarBuilder: (navBarConfig) => Style1BottomNavBar(
+          navBarConfig: NavBarConfig(
+            selectedIndex: navBarConfig.selectedIndex,
+            items: navBarConfig.items,
+            onItemSelected: (int index) {
+              if (index == 1) {
+                Get.toNamed('/cart');
+                return;
+              }
+              navBarConfig.onItemSelected(index);
             },
-            child: Text("Home Page")),
+          ),
+        ),
       ),
     );
   }
